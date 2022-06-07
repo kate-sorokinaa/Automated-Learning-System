c294da36f5d6f99bef8be28fd9a38bb15a9b95f1
kate-sorokinaa <kate_sorokinaa@mail.ru>
2022-06-07 13:36:48 +0300

    Improved menu

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+
+import courses.views
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
-    path('courses/', views.about, name='courses'),
-    path('profile/', views.about, name='profile'),
+    path('courses/', courses.views.courses_home, name='courses'),
     path('login/', views.user_login, name='login'),
     path('register/', views.RegisterFormView.as_view(), name='register'),
 ]
